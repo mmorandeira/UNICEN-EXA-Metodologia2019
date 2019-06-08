ba96c005e68425ac93ac2b80ae3a5d565adfafe8
Mattii97 <Morandeiramatias@gmail.com>
2019-06-08 17:43:05 -0300

    meee.

--- src/backend/UserManager.java
+++ src/backend/UserManager.java
@@ -1,4 +1,27 @@
 package backend;
 
+import java.util.HashMap;
+
 public class UserManager {
+    private HashMap<String,User> usuarios;
+    static UserManager instance;
+
+    private UserManager(){
+        this.usuarios = new HashMap<String,User>();
+    }
+
+    public static UserManager getInstance() {
+        if(instance == null)
+            instance = new UserManager();
+        return instance;
+    }
+
+    public void addUser(User usuario){
+        usuarios.put(usuario.getMail(),usuario);
+    }
+
+    public boolean validarUsuario(String mail, String password){
+        return usuarios.containsKey(mail) && usuarios.get(mail).getPassword().equals(password);
+    }
+
 }
---
+++ src/etc/User.java
@@ -0,0 +1,4 @@
+package etc;
+
+public class User {
+}
