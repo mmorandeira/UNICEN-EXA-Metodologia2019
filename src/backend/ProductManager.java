24d17ae47fb291a37ec59d74c2dd9f8aa16b1679
Juampico <jcorrea@alumnos.exa.unicen.edu.ar>
2019-06-09 18:44:52 -0300

    Clase Product Manager

--- src/backend/ProductManager.java
+++ src/backend/ProductManager.java
@@ -1,4 +1,30 @@
 package backend;
 
-public class ProductManager {
+Public class ProductManager{
+	
+	private HashMap<String, Product> productos;
+    static ProductManager instance;
+	
+    private ProductManager() {
+        this.productos = new HashMap<String, Product>();
+    }
+	
+    public static ProductManager getInstance() {
+        if (instance == null)
+            instance = new ProductManager();
+        return instance;
+    }
+
+    public void addProduct(Product producto) {
+        productos.put(producto.getCodigoDeBarra(), producto);
+    }
+ 
+    public boolean existeProducto(String codigodebarra) {
+        return productos.conteinskey(codigodebarra);
+    }    
+    
+    public Product getProducto(String codigodebarra) {
+        return productos.get(producto.getCodigoDeBarra());
+    }       
+    
 }
