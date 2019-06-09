0f533595f34fee68b8e0cae167fa2dfd125b36b0
Juampico <jcorrea@alumnos.exa.unicen.edu.ar>
2019-06-09 17:41:11 -0300

    Actualizacion UserApp

--- src/UserApp.java
+++ src/UserApp.java
@@ -1,15 +1,49 @@
 import backend.UserManager;
 import etc.User;
+import java.time.LocalDate;
 
 public class UserApp {
     private UserManager userManager;
     private boolean logeado;
     private User usuario;
     private Mapa mapa;
+    private ProductManager productManager;
 
     public UserApp(){
-        userManager=UserManager.getInstance();
+        this.userManager=UserManager.getInstance();
+        this.mapa = Mapa.getIntance();
+        this.productManager = ProductManager.getInstance();
+        this.logeado = false;
+        this.usuario = usuario;
     }
 
+    public void login(String mail, String pass) {
+    	if (this.userManager.existeUser(mail)) {
+    		if (this.userManager.validarPassword(pass)) {
+    			this.logeado = true;
+    			this.usuario = this.userManager.getUser(mail);
+    			this.usuario.addActivity(LocalDate.now());
+    		}
+    	}
+    }
+    
+    public void verMapa() {
+    	this.mapa.mostrar();
+    }
+    
+    public double calcularEstadisticas() {
+    	return this.usuario.getPromedio();
+    }
 
+    public void verAhorro() {
+    	
+    }
+    
+    public void addProduct(String codBarras, int cant) {
+    	if (this.productManager.existeProduct(codBarras)) {
+    		Pair <this.productManager.getProduct(codBarras), cant> pAux;
+    		this.usuario.addAcopio(pAux);
+    	}
+    }
+    
 }
