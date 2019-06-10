7e95254db911126ba9beef1497d1cef3d5cd6acb
Juampico <jcorrea@alumnos.exa.unicen.edu.ar>
2019-06-10 17:03:47 -0300

    se agregaron funciones basicas pto itinerante

--- src/etc/PuntoItinerante.java
+++ src/etc/PuntoItinerante.java
@@ -1,7 +1,42 @@
 package etc;
 
 public class PuntoItinerante {
-    public PuntoItinerante(){
-
+    
+	private int id;
+	private String direccion;
+	private boolean activo;
+	//private Camion c;
+	
+	public PuntoItinerante(int id, String direccion, boolean activo){
+		this.id = id; 
+		this.direccion = direccion;
+		this.activo = activo;
     }
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public boolean isActivo() {
+		return activo;
+	}
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+    
+	
+	
 }
