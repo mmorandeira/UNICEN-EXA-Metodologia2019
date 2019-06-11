50564b0ffc2d9cf2f43f930b399bbf54af506744
Mattii97 <Morandeiramatias@gmail.com>
2019-06-11 17:09:10 -0300

    Se actulizo la userApp.

--- src/frontend/UserApp.java
+++ src/frontend/UserApp.java
@@ -6,6 +6,7 @@ import util.Pair;
 import java.time.LocalDate;
 import backend.ProductManager;
 import backend.Mapa;
+import backend.Municipio;
 
 public class UserApp {
     private UserManager userManager;
@@ -13,6 +14,7 @@ public class UserApp {
     private User usuario;
     private Mapa mapa;
     private ProductManager productManager;
+    private Municipio municipio;
 
     public UserApp(){
         this.userManager=UserManager.getInstance();
@@ -20,62 +22,64 @@ public class UserApp {
         this.productManager = ProductManager.getInstance();
         this.logeado = false;
         this.usuario = null;
+        this.municipio = Municipio.getInstance();
     }
 
     public void login(String mail, String pass) {
-    	if ((this.userManager.existeUser(mail)) && !this.logeado) {
-    		if (this.userManager.validarPassword(mail,pass)) {
-    			this.logeado = true;
-    			this.usuario = this.userManager.getUser(mail);
-    			this.usuario.addActivity(LocalDate.now());
-    		}
-    	}
-    }
-    
-    public void verMapa() {
-    	this.mapa.mostrar();
-    }
-    
-    public double calcularEstadisticas() {
-    	if (this.logeado)
-    		return this.usuario.getCantEnPeso();
-    	return 0;
+        if ((this.userManager.existeUser(mail)) && !this.logeado) {
+            if (this.userManager.validarPassword(mail,pass)) {
+                this.logeado = true;
+                this.usuario = this.userManager.getUser(mail);
+                this.usuario.addActivity(LocalDate.now());
+            }
+        }
     }
 
-    
+    public void verMapa() {
+        this.mapa.mostrar();
+    }
+
+    public double calcularEstadisticas() {
+        if (this.logeado)
+            return this.usuario.getCantEnPeso();
+        return 0;
+    }
+
+
     public void signIn(String nombre, String apellido, String mail, String pass, String domicilio, String barrio, int dni) {
-    	if (!this.userManager.existeUser(mail) && !this.logeado) {
-    		User aux = new User(nombre, apellido, domicilio, barrio, pass, mail, dni);
-    		userManager.addUser(aux);
-			login(mail, pass);
-    	}
+        if (!this.userManager.existeUser(mail) && !this.logeado) {
+            User aux = new User(nombre, apellido, domicilio, barrio, pass, mail, dni);
+            userManager.addUser(aux);
+            login(mail, pass);
+        }
     }
-    
-    
+
+
     public void logOut() {
-    	this.usuario = null;
-    	this.logeado = false;
+        this.usuario = null;
+        this.logeado = false;
     }
-    
+
     public void verAhorro() {
-    	// Hacer simulacion, mensajes con municipio
+        // Hacer simulacion, mensajes con municipio
     }
-    
-    
+
+
     public void verCronograma() {
-    	// Hacer simulacion, mensajes con municipio
+        // Hacer simulacion, mensajes con municipio
     }
-    
+
     public void verAyudaONG() {
-    	// Hacer simulacion, mensajes con municipio
+        // Hacer simulacion, mensajes con municipio
     }
-    
+
     public void addProduct(String codBarras, Integer cant) {
-    	if ((this.productManager.existeProducto(codBarras)) && this.logeado) {
-    		Product p = this.productManager.getProducto(codBarras);
+        if ((this.productManager.existeProducto(codBarras)) && this.logeado) {
+            Product p = this.productManager.getProducto(codBarras);
             Pair<Product, Integer> pAux = new Pair<Product, Integer>(p,cant);
-    		this.usuario.addAcopio(pAux);
-    	}
+            this.usuario.addAcopio(pAux);
+            this.municipio.addAcopio(this.usuario, pAux);
+        }
     }
-    
+
 }
